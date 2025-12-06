6fea765cecf48f10b039002fee408fd5aa4cc734
yuta <yu.code.tech@gmail.com>
2025-12-06 21:11:39 +0900

    refactor: UserNameField のスタイル調整

--- src/constants/UserNameField/index.tsx
+++ src/constants/UserNameField/index.tsx
@@ -9,13 +9,15 @@ interface UserNameFieldProps {
 export const UserNameField = ({ errorMessage, isError = false }: UserNameFieldProps) => {
   return (
     <Field>
-      <FieldLabel htmlFor="username">ユーザー名</FieldLabel>
-      <FieldDescription className="text-xs">名前には文字、数字、アンダースコアのみ使用できます</FieldDescription>
+      <FieldLabel htmlFor="username" className="text-base">
+        ユーザー名
+      </FieldLabel>
+      <FieldDescription>名前には半角英数字とアンダーバー記号のみ使用できます</FieldDescription>
       <InputGroup>
         <InputGroupInput required id="username" placeholder="Yamada" aria-invalid={isError} />
         <InputGroupAddon>@</InputGroupAddon>
       </InputGroup>
-      {isError && <FieldError className="text-xs">{errorMessage}</FieldError>}
+      {isError && <FieldError>{errorMessage}</FieldError>}
     </Field>
   )
 }
