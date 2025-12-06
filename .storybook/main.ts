b7ec69ca6cc878316b75a89b2830c64165d5b44e
yuta <yu.code.tech@gmail.com>
2025-12-06 18:29:47 +0900

    chore: フォーマット

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,9 @@
-import type { StorybookConfig } from '@storybook/nextjs-vite';
+import type { StorybookConfig } from '@storybook/nextjs-vite'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "../public"
-  ]
-};
-export default config;
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [],
+  framework: '@storybook/nextjs-vite',
+  staticDirs: ['../public']
+}
+export default config
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,5 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { defineConfig, globalIgnores } from 'eslint/config'
+import storybook from 'eslint-plugin-storybook'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
 import prettier from 'eslint-config-prettier'
@@ -9,6 +7,7 @@ import prettier from 'eslint-config-prettier'
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...storybook.configs['flat/recommended'],
   prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
