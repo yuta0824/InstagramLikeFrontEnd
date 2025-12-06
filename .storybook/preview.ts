cbed623c63a64762c5ba8acfb5ac2c2302c463b8
yuta <yu.code.tech@gmail.com>
2025-12-06 18:10:33 +0900

    chore: ベースCSSのセットアップ

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,15 @@
 import type { Preview } from '@storybook/nextjs-vite'
+import '@/styles/globals.css'
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
-};
+        color: /(background|color)$/i,
+        date: /Date$/i
+      }
+    }
+  }
+}
 
-export default preview;
+export default preview
