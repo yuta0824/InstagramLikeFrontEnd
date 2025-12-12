89e2116b025241f51b11e14279f120781f71fc92
yuta <yu.code.tech@gmail.com>
2025-12-12 22:10:41 +0900

    fix: サイドバーを固定

--- src/components/layout/GlobalNavigation/index.tsx
+++ src/components/layout/GlobalNavigation/index.tsx
@@ -39,7 +39,7 @@ const menuItems = [
 
 export const GlobalNavigation = ({ name, email, myPageUrl, avatarUrl, onLogout }: GlobalNavigationProps) => {
   return (
-    <nav className="fixed right-0 bottom-0 left-0 z-50 border-t border-gray-200 bg-white backdrop-blur-sm md:static md:h-screen md:w-16 md:border-t-0 md:border-r md:px-2 md:py-6 xl:w-64 xl:px-4">
+    <nav className="fixed right-0 bottom-0 left-0 z-50 border-t border-gray-200 bg-white backdrop-blur-sm md:sticky md:top-0 md:h-screen md:w-16 md:border-t-0 md:border-r md:px-2 md:py-6 xl:w-64 xl:px-4">
       <div className="mx-auto flex max-w-80 flex-row items-center justify-around py-2 md:h-full md:flex-col md:justify-start md:gap-3 md:py-0">
         {menuItems.map(item => {
           const Icon = item.icon
