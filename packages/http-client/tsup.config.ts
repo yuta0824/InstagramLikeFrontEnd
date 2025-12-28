8b30d96b1472a1e6f9436c121af54d6d7302019c
yuta <yu.code.tech@gmail.com>
2025-12-28 22:45:40 +0900

    chore: OpenAPI Generator setup

---
+++ packages/http-client/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  tsconfig: "tsconfig.json",
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  target: "es2020",
+});
