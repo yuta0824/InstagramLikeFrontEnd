8a23d9dde182897dbc7d86f3a5463e3ce42d477b
yuta <yu.code.tech@gmail.com>
2025-12-30 18:26:03 +0900

    feat: ログアウトAPI結合

--- src/features/layout/views/GlobalNavigationContainer.tsx
+++ src/features/layout/views/GlobalNavigationContainer.tsx
@@ -1,17 +1,23 @@
 'use client'
 
 import { GlobalNavigation } from '@/components/layout/GlobalNavigation'
+import { useLogout } from '@/features/auth/api/useLogout'
+import { deleteJwtFromCookie } from '@/features/auth/modules/deleteJwtFromCookie'
 import { useGetMe } from '@/features/user/api/useGetMe'
+import { useRouter } from 'next/navigation'
 
 export const GlobalNavigationContainer = () => {
   const { data, error, isLoading } = useGetMe()
-  if (!data || error) return
+  const { logoutMutation } = useLogout()
+  const router = useRouter()
+  if (!data || !logoutMutation || error) return null
 
   const name = data?.name
   const myPageUrl = `/accounts/${data?.name}`
   const handleLogout = () => {
-    // TODO: ログアウトの実装
-    console.log('logout')
+    logoutMutation.mutate()
+    deleteJwtFromCookie()
+    router.push('/')
   }
 
   return isLoading ? <p>Loading...</p> : <GlobalNavigation name={name} myPageUrl={myPageUrl} onLogout={handleLogout} />
