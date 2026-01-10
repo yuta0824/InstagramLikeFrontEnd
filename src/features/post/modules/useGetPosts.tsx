bd3481a47f1498249b05bee29f95582793a1265d
yuta <yu.code.tech@gmail.com>
2026-01-11 01:24:12 +0900

    feat: useGetPosts hook

---
+++ src/features/post/modules/useGetPosts.tsx
@@ -0,0 +1,30 @@
+import { getJwtFromCookie } from '@/features/auth/modules/getJwtFromCookie'
+import { postApi } from '@instagram-like-app/http-client'
+import { useQuery } from '@tanstack/react-query'
+
+export const useGetPosts = () => {
+  const jwt = getJwtFromCookie()
+
+  const fetchGetPosts = async () => {
+    return await postApi
+      .apiPostsGet({
+        headers: { Authorization: `Bearer ${jwt}` }
+      })
+      .catch(error => {
+        console.error(error)
+        throw new Error('エントリー記事の取得に失敗しました。')
+      })
+  }
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['getPosts'],
+    queryFn: fetchGetPosts,
+    enabled: !!jwt
+  })
+
+  return {
+    data,
+    isLoading,
+    error
+  }
+}
